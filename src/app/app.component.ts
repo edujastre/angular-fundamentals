b721ac1e04c989b44218281c881763b93d3835ac
edujastre <edujastre@yahoo.com.br>
2018-07-24 00:29:19 -0300

    Angular fundamentals promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from "./git-search.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(private GitSearchService: GitSearchService) {
+    
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.GitSearchService.gitSearch('rxjs')
+    .then((response) => {
+      alert("Total Library found " + response.total_count);
+    }, (error) => {
+      alert("error:" + error.statusText )
+    })    
+  }
+  title = 'app is functional';
 }
